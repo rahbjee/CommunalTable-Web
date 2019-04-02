371dbeaf56d8598f1b3c493a37e7c75f1282d8ab
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2019-04-02 17:49:50 -0400

    added user_id & seats in events, rendering info in meals.html template

--- app.py
+++ app.py
@@ -10,18 +10,35 @@ def index():
 @app.route('/meals.html')
 def meals():
     mealsList = []
+    hostList = []
+    userIDList = []
     conn = sqlite3.connect('communaltable.db')
     cur = conn.cursor()
     queryAllMeals = '''SELECT * FROM events'''
     cur.execute(queryAllMeals)
     for row in cur:
+        print(row)
         mealDict = {}
         mealDict["description"] = row[12]
         mealDict["address1"] = row[1]
         mealDict["zipcode"] = row[5]
+        mealDict["seats"] = row[19]
+        userID = row[18]
+        mealDict['host_id'] = userID
+        userIDList.append(userID)
         mealsList.append(mealDict)
+    for i in userIDList:
+        queryHost = "SELECT firstname, lastname FROM users WHERE users_id =" + str(i)
+        cur.execute(queryHost)
+        for row in cur:
+            fullname = row[0] + " " + row[1]
+            hostList.append(fullname)
+    index = 0
+    for meal in mealsList:
+        meal['host'] = hostList[index]
+        index = index+1
+
     conn.close()
-    hostList = ['Bob', 'Bri']
     return render_template('meals.html',title="Meals Around", meals = mealsList)
 
 
