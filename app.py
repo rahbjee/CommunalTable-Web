053059225728459597dc8dc201e6fb0f23c1d5e4
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2019-04-15 21:21:19 -0400

    adding front-end and rendering mealList in head

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import sqlite3
 from wtforms import Form, SubmitField, IntegerField, HiddenField, validators
 app = Flask(__name__)
@@ -12,8 +12,53 @@ class RSVPForm(Form):
 
 @app.route('/')
 def index():
-    return '''<h1>Welcome to Communal Table!</h1>
-            <nav><ul><li><a href="/meals.html">Meals</a></li><li><a href="/host.html">Host</a></li></ul></nav>'''
+    mealsList = []
+    hostList = []
+    userIDList = []
+    conn = sqlite3.connect('communaltable.db')
+    cur = conn.cursor()
+    queryAllMeals = '''SELECT * FROM events'''
+    cur.execute(queryAllMeals)
+    for row in cur:
+        mealDict = {}
+        mealDict["meal_id"] = row[0]
+        mealDict["description"] = row[12]
+        mealDict["address1"] = row[1]
+        mealDict["zipcode"] = row[5]
+        mealDict["seats"] = row[19]
+        mealDict["name"] = row[21]
+        time = row[20]
+        mealDict['date'] = time[0:10]
+        mealDict['time'] = time[11:16]
+        userID = row[18]
+        userIDList.append(userID)
+        mealsList.append(mealDict)
+    for i in userIDList:
+        queryHost = "SELECT firstname, lastname FROM users WHERE users_id =" + str(i)
+        cur.execute(queryHost)
+        for row in cur:
+            fullname = row[0] + " " + row[1]
+            hostList.append(fullname)
+    index = 0
+    for meal in mealsList:
+        meal['host'] = hostList[index]
+        index = index+1
+    if request.method == "POST" and rsvpform.validate():
+        rsvp_num = request.form.get('rsvpNumber')
+        rsvp_meal = request.form.get('rsvpMealID')
+        querySeats = "SELECT seats FROM events WHERE events_id =" + rsvp_meal
+        cur.execute(querySeats)
+        for seat in cur:
+            mealCap = seat[0]
+        if int(rsvp_num) > mealCap:
+            message = "Error!"
+            print(message)
+        else:
+            queryRSVP = "UPDATE events SET seats = " + str(mealCap-int(rsvp_num))+ " WHERE events_id = " + rsvp_meal
+            cur.execute(queryRSVP)
+    conn.commit()
+    conn.close()
+    return render_template('index.html', meals = mealsList)
 
 @app.route('/meals.html', methods = ['GET', 'POST'])
 def meals():
@@ -32,6 +77,7 @@ def meals():
         mealDict["address1"] = row[1]
         mealDict["zipcode"] = row[5]
         mealDict["seats"] = row[19]
+        mealDict["name"] = row[21]
         time = row[20]
         y = time[0:4]
         m = time[5:7]
